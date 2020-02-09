6e2aca99600d4ff48e4f28102cddb8d9241e6bfa
Kelley Chaplain <kmchaplain@gmail.com>
2020-02-09 15:47:35 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |k, v|
+    v.each do |new_v, names|
+      names.each do |name|
+
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+
+        if !pigeons[name][k]
+          pigeons[name][k] = []
+        end
+
+        pigeons[name][k] << new_v.to_s
+
+      end
+    end
+  end
+  pigeons  
 end
