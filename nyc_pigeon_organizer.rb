30e715682277d415ba23b6a615729b1bce2aa856
Kelley Chaplain <kmchaplain@gmail.com>
2020-07-19 20:24:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,10 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |key, value|
     value.each do |k, v|
       v.each do |pig|
-        if !bird_box.has_key?(pig)
+        # if !bird_box.has_key?(pig)
+        binding.pry
           bird_box[pig] = {color: [], gender: [], lives: []}
-        end
-        # binding.pry
+        # end
       end
     end
   end
